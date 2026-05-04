0974b81cf80ca473a8018fa5b5bcc06c5fe19986
Sonu Kumar <sonuec12@gmail.com>
2026-05-04 17:12:26 +0530

    Add: top-k-elements-in-list - submission-14

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-14.js
@@ -0,0 +1,38 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @param {number} k
+   * @return {number[]}
+   */
+  topKFrequent(nums, k) {
+
+    const freq = {};
+    for (const num of nums) {
+      freq[num] = (freq[num] || 0) + 1;
+    }
+
+    // add top k to min heap
+    const heap = new MinPriorityQueue(a => a[0]);
+
+    for (const num in freq) {
+      heap.enqueue([freq[num], num]);
+
+      // IMP : dequeue only after pushing the new. As this will dequeue the node with lowest priority
+      if(heap.size() > k){
+        heap.dequeue();
+      }
+
+    }
+
+    const res = [];
+    while (!heap.isEmpty()) {
+      console.log(heap.size())
+      let [_, num] = heap.dequeue();
+      res.push(num);
+    }
+
+    return res;
+
+
+  }
+}
