063e776fba0fa3a5db6014b0f79667f44cae0912
Sonu Kumar <sonuec12@gmail.com>
2026-05-08 23:44:37 +0530

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let res = 0;
+
+        let l = 0;
+        let r = height.length - 1;
+        let leftMax = height[l];
+        let rightMax = height[r];
+
+        while(l < r){
+            if(leftMax < rightMax){
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                res += leftMax - height[l];
+                
+            }else{
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                res += rightMax - height[r];
+            }
+        }
+
+        return res;
+    }
+}
