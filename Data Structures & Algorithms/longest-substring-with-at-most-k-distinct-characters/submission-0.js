f617fd09e3735d0413a1c35f8fa30693cf591007
Sonu Kumar <sonuec12@gmail.com>
2026-05-19 23:43:26 +0530

    Add: longest-substring-with-at-most-k-distinct-characters - submission-0

---
+++ Data Structures & Algorithms/longest-substring-with-at-most-k-distinct-characters/submission-0.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    lengthOfLongestSubstringKDistinct(s, k) {
+        let map = {};
+        let res = 0;
+
+        let l = 0,
+            r = 0;
+        
+        while(r < s.length){
+            let c = s[r];
+
+            // add
+            map[c] = (map[c] || 0) + 1;
+
+            // invalid window
+            while(Object.keys(map).length > k){
+                // keep removing from left till it becomes valid again
+                map[s[l]]--;
+                if(map[s[l]] <= 0){
+                    delete map[s[l]];
+                }
+                l++;
+            }
+
+            // update answer
+            res = Math.max(res, r - l + 1);
+
+            r++;
+        }
+
+        return res;
+
+
+    }
+}
