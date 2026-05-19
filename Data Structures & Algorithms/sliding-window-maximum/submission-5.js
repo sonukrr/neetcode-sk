5055bed65f0b8effa869fbf35817d6d8425c97ef
Sonu Kumar <sonuec12@gmail.com>
2026-05-19 21:02:38 +0530

    Add: sliding-window-maximum - submission-5

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-5.js
@@ -0,0 +1,16 @@
+class Solution {
+    maxSlidingWindow(nums, k) {
+        const res = [];
+        let l = 0;
+        let max = -Infinity;
+        let r = k - 1;
+        while (r < nums.length) {
+            let num = nums.slice(l, r + 1);
+            res.push(Math.max(max, ...num));
+
+            l++;
+            r++;
+        }
+        return res;
+    }
+}
