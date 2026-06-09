3af406047611ff4faeccbac76ec0de43835a12ef
Sonu Kumar <sonuec12@gmail.com>
2026-06-09 23:31:48 +0530

    Add: linked-list-cycle-detection - submission-8

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-8.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        if(!head) return false;
+        
+        let slow = head;
+        let fast = head.next;
+
+        while(fast && fast.next){
+            if(slow == fast){
+                return true;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+}
