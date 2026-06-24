830db826cc7b2ca2f4cff24d1265bb5ba33b4de7
Sonu Kumar <sonuec12@gmail.com>
2026-06-24 13:09:27 +0530

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+
+        // adjacency list
+        const map = {};
+        for(let i = 0; i < numCourses; i++){
+            map[i] = [];
+        }
+
+        for(const [c1, c2] of prerequisites){
+            map[c1].push(c2)
+        }
+
+        for(let i = 0; i < numCourses; i++){
+            if(hasCycle(i, new Set())){
+                return false;
+            }
+           
+        }
+
+        return true;
+
+        function hasCycle(c, set){
+            if(set.has(c))
+                return true;
+
+            if(map[c].length == 0)
+                return false;
+
+            set.add(c);
+            
+            for(const nc of map[c]){
+                if(hasCycle(nc, set))
+                    return true;
+            }
+
+            set.delete(c);
+            map[c] = [];
+
+            return false;
+        }
+    }
+}
